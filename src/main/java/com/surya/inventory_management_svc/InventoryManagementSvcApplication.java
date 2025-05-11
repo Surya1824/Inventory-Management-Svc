ab2b6259362b3d1de003c95bfecf24783e403d28
Surya1824 <suryateja5347@gmail.com>
2025-05-11 19:11:18 +0530

    Handle stock management and updates when orders are placed or canceled.

---
+++ src/main/java/com/surya/inventory_management_svc/InventoryManagementSvcApplication.java
@@ -0,0 +1,13 @@
+package com.surya.inventory_management_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surya/inventory_management_svc/InventoryManagementSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surya.inventory_management_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
